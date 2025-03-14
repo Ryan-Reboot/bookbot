e2989501c9fb1378bf05d52acb6586c325e6a987
Ryan_Reboot <rfgwest@gmail.com>
2025-03-14 01:34:45 -0700

    D: move get_word_count function to stats.py file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+from stats import get_word_count
+
 # create a function that takes a path to file and reads the text of the file into a string
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_word_count():
-    book = get_book_text("books/frankenstein.txt")
-    words_list = book.split()
-    num_words = len(words_list)
-    return(num_words)
-
 # create a main function, which returns a word count from the book
 def main():
-    print(f"{get_word_count()} words found in the document")
+    book = get_book_text("books/frankenstein.txt")
+    words = get_word_count(book)
+    print(f"{words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+# a function that counts the words in a given text
+def get_word_count(text):
+    words_list = text.split()
+    num_words = len(words_list)
+    return(num_words)
+
