38adcd4dcd23f7a0857a0669c8ce9e6424af10f0
Ryan_Reboot <rfgwest@gmail.com>
2025-03-14 00:35:12 -0700

    C: mod main.py to count words

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-# create a main function, which provides the path for the get_book_text function
+# create a main function, which returns a word count from the book
 def main():
-    result = get_book_text("books/frankenstein.txt")
-    print(result)
+    book = str(get_book_text("books/frankenstein.txt"))
+    words_list = book.split()
+    num_words = len(words_list)
+    print(f"{num_words} words found in the document")
 
 main()
