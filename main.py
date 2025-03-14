f2ef63d964b77fcffffebe7321575458f8e00c35
Ryan_Reboot <rfgwest@gmail.com>
2025-03-14 01:15:40 -0700

    C2: fixed main.py counting function

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-# create a main function, which returns a word count from the book
-def main():
-    book = str(get_book_text("books/frankenstein.txt"))
+def get_word_count():
+    book = get_book_text("books/frankenstein.txt")
     words_list = book.split()
     num_words = len(words_list)
-    print(f"{num_words} words found in the document")
+    return(num_words)
+
+# create a main function, which returns a word count from the book
+def main():
+    print(f"{get_word_count()} words found in the document")
 
 main()
